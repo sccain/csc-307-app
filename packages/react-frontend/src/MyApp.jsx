c7407f73e8abbad8d5749f0674956b3fdf454ddc
sccain <sccain@calpoly.edu>
2024-10-10 18:51:00 -0700

    Added ID column to table

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -29,7 +29,28 @@ function MyApp() {
 			.then((json) => setCharacters(json["users_list"]))
 			.catch((error) => { console.log(error); });
 	      }, [] );
-	      
+	
+	function postUser(person) {
+		const promise = fetch("Http://localhost:8000/users", {
+		  method: "POST",
+		  headers: {
+		    "Content-Type": "application/json",
+		  },
+		  body: JSON.stringify(person),
+		});
+	    
+		return promise;
+	      }
+
+ 
+	function updateList(person) { 
+		postUser(person)
+		  .then(() => setCharacters([...characters, person]))
+		  .catch((error) => {
+		    console.log(error);
+		  })
+	    }
+
 	return (
 		<div className="container">
 		  <Table
